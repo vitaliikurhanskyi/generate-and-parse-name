1228898dc4a9f2354e4e748fb47805b193ea9189
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-07-04 15:50:39 +0300

    04.07.2022

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,13 @@ function setCountriesForBtn() {
 
 let chooseCountry = document.querySelectorAll('.select-country');
 
-let deffaultCountry = 'BE';
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+let randomCountry = getRandomInt(countriesList.countries.length);
+
+let deffaultCountry = countriesList.countries[randomCountry].countryCode;
 
 let buttonSelectCountryName = document.querySelector('.btn-inner-text');
 
@@ -34,20 +40,37 @@ function inputNameValue(data) {
 	document.querySelector('#last-name').value = data.data[0].name.lastname.name;
 }
 
+function Error(text) {
+	if ((window.navigator.onLine && document.querySelector(".internet-contection-error") || document.querySelector(".internet-contection-error"))) {
+		document.querySelector(".internet-contection-error").remove();
+	}
+	let interneOffline = document.createElement("div");
+	interneOffline.setAttribute("class", "internet-contection-error");
+	interneOffline.innerHTML = `<div class="alert alert-danger" role="alert">${text}</div>`;
+	document.querySelector(".container").before(interneOffline);
+	return;
+}
+
 let generateNameButton = document.querySelector('#generate-name');
 
 generateNameButton.onclick = function () {
+
+
 	if (!window.navigator.onLine) {
-		let interneOffline = document.createElement("div");
-		interneOffline.setAttribute("class", "internet-contection-error");
-		interneOffline.innerHTML = `<div class="alert alert-danger" role="alert">Internet Connection Error!</div>`;
-		document.querySelector(".container").before(interneOffline);
+		Error("Internet Connection Problem!");
 		return;
+	}
+
+	if (window.navigator.onLine && document.querySelector('.internet-contection-error')) {
+		document.querySelector('.internet-contection-error').remove();
 	}
 
 	fetch(`https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=generate&country_code=${deffaultCountry}`)
 		.then(data => {
-			return data.json();
+			if (data.ok) {
+				return data.json();
+			}
+			console.log(data.ok);
 		})
 		.then(data => {
 			inputNameValue(data);
@@ -63,41 +86,56 @@ let firstName = document.querySelector("#first-name");
 
 let lastName = document.querySelector("#last-name");
 
-btnSubmitHeader.onclick = function () {
-	if (firstName.value && document.querySelector("#firstNameEror")) {
-		document.querySelector("#firstNameEror").remove();
+function inputEmptyNameError(attributeName, errorText, insertFor) {
+	if (document.getElementById(attributeName)) {
+		document.getElementById(attributeName).remove();
 	}
+	let firstNameError = document.createElement("span");
+	firstNameError.setAttribute("id", attributeName);
+	firstNameError.style.color = "red";
+	firstNameError.innerHTML = `<div class="header-alerts alert alert-danger" role="alert">${errorText}</div>`;
+	insertFor.after(firstNameError);
+}
 
-	if (lastName.value && document.querySelector("#lastNameEror")) {
-		document.querySelector("#lastNameEror").remove();
-	}
-	if (firstName.value === '' || firstName.value === null) {
-		if (document.querySelector("#firstNameEror")) {
-			document.querySelector("#firstNameEror").remove();
-		}
-		let firstNameError = document.createElement("span");
-		firstNameError.setAttribute("id", "firstNameEror");
-		firstNameError.style.color = "red";
-		firstNameError.innerHTML = `<div class="header-alerts alert alert-danger" role="alert">Please Enter Your First Name!!!</div>`;
-		firstName.after(firstNameError);
-	}
-	if (lastName.value === '' || lastName.value === null) {
-		if (document.querySelector("#lastNameEror")) {
-			document.querySelector("#lastNameEror").remove();
-		}
-		let lastNameError = document.createElement("span");
-		lastNameError.setAttribute("id", "lastNameEror");
-		lastNameError.style.color = "red";
-		lastNameError.innerHTML = `<div class="header-alerts alert alert-danger" role="alert">Please Enter Your Last Name!!!</div>`;
-		lastName.after(lastNameError);
-	}
+btnSubmitHeader.onclick = function () {
+	if (firstName.value && document.querySelector("#firstNameEror")) document.querySelector("#firstNameEror").remove();
+
+	if (lastName.value && document.querySelector("#lastNameEror")) document.querySelector("#lastNameEror").remove();
+
+	if (firstName.value === '' || firstName.value === null) inputEmptyNameError("firstNameEror", "Please Enter Your First Name!!!", firstName);
+
+	if (lastName.value === '' || lastName.value === null) inputEmptyNameError("lastNameEror", "Please Enter Your Last Name!!!", lastName);
 
 	if (lastName.value && firstName.value) {
-		console.log('go');
+		fetch(`https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=parse&name=${firstName.value}%20${lastName.value}&country_code=${deffaultCountry}`)
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				if (data.results == 0) {
+					Error(data.error);
+					return;
+				}
+				if (data.results == 1) cardData(data);
+			}).catch(error => {
+				console.log(error);
+			});
 	}
+}
+
+
+function cardData(data) {
+	let fullName = document.querySelector('#person-full-name');
+	let demonym = document.querySelector('#demonym');
+	let firstName = data.data[0].name.firstname.name;
+	let lastName = data.data[0].name.lastname.name;
+	let salutation = data.data[0].salutation.salutation;
+	fullName.innerText = `${salutation} ${firstName} ${lastName}`;
+	demonym.innerText = data.data[0].country.demonym;
 }
 
 // #validate and parse inputs
 
 // https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=parse&name=John%20Bruch&country_code=AU
 
+// https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=parse&name=faefwef%20awefawef&country_code=AU
