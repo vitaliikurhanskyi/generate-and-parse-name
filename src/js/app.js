35c5b20c2c45154ae5e6dd390eb50edb44f1455d
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-07-07 20:13:27 +0300

    07.07

--- src/js/app.js
+++ src/js/app.js
@@ -125,43 +125,79 @@ btnSubmitHeader.onclick = function () {
 
 	if (lastName.value === '' || lastName.value === null) inputEmptyNameError("lastNameEror", "Please Enter Your Last Name!!!", lastName);
 
+	if (!window.navigator.onLine) {
+		Error("Internet Connection Problem!");
+		return;
+	}
+
 	if (lastName.value && firstName.value) {
-		fetch(`https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=parse&name=${firstName.value}%20${lastName.value}&country_code=${deffaultCountry}`)
-			.then(data => {
-				return data.json();
-			})
-			.then(data => {
-				if (data.error) Error(data.error);
-				if (data.results == 0) {
-					Error(data.error);
-					return;
-				}
-				if (data.results == 1) cardData(data); console.log(data);
-			});
+
+		this.classList.add("btn-secondary");
+		this.setAttribute("disabled", "")
+
+		let randomPerson = new Promise((resolve, reject) => {
+			fetch(`https://api.parser.name/?api_key=c3cbb6b73739c61bebeb7cbdba7bffff&endpoint=parse&name=${firstName.value}%20${lastName.value}&country_code=${deffaultCountry}`)
+				.then(data => {
+					resolve(data.json());
+				});
+		});
+
+		let randomImage = new Promise((resolve, reject) => {
+			fetch(`https://random.imagecdn.app/500/150`)
+				.then(image => {
+					resolve(image);
+				});
+		});
+
+		Promise.all([randomPerson, randomImage]).then(data => {
+			//this.classList.remove("btn-secondary");
+			//this.removeAttribute("disabled");
+			if (data[0].error) Error(data[0].error);
+			if (data[0].results == 0) Error(data[0].error);
+			if (!data[1].ok) Error("Image doesn't load");
+			if (data) cardSetData(data);
+		});
+
 	}
 }
 
-function cardData(data) {
 
-	let firstName = data.data[0].name.firstname.name;
-	let lastName = data.data[0].name.lastname.name;
-	let salutation = data.data[0].salutation.salutation;
-	let gender = data.data[0].name.firstname.gender_formatted;
-	let countryDemonym = data.data[0].country.demonym;
-	let country = data.data[0].country.name;
-	let continent = data.data[0].country.continent;
-	let language = data.data[0].country.primary_language;
-
-	let fullName = document.querySelector('#person-full-name');
-	let demonym = document.querySelector('#demonym');
-	let cardCountry = document.querySelector('#card-country');
-	let cardLanguage = document.querySelector('#card-language');
+let fullName = document.querySelector('#person-full-name');
+let demonym = document.querySelector('#demonym');
+let cardCountry = document.querySelector('#card-country');
+let cardLanguage = document.querySelector('#card-language');
+let cardImage = document.querySelector('#card-image');
 
 
+function cardSetData(data) {
+
+	console.log(data);
+
+
+	let firstName, lastName, salutation, gender, countryDemonym, country, continent, language, image;
+
+	firstName = data[0].data[0].name.firstname.name;
+	lastName = data[0].data[0].name.lastname.name;
+	salutation = data[0].data[0].salutation.salutation;
+	data[0].data[0].name.firstname.gender_formatted;
+	countryDemonym = data[0].data[0].country.demonym;
+	country = data[0].data[0].country.name;
+	continent = data[0].data[0].country.continent;
+	language = data[0].data[0].country.primary_language;
+	image = data[1].url;
+
+	cardImage.src = image;
 	fullName.innerHTML = `<span class="text-color-blue">${salutation} ${firstName} ${lastName}</span>`;
 	demonym.innerHTML = `<span class="text-color-orange">${gender} - ${countryDemonym}</span>`;
 	cardCountry.innerHTML = `<span class="text-color-blue">Country:</span>  <span class="text-color-orange">${country} - ${continent}</span>`;
 	cardLanguage.innerHTML = `<span class="text-color-blue">Primary Language:</span>  <span class="text-color-orange">${language}</span>`;
+
+
+	btnSubmitHeader.classList.remove("btn-secondary");
+	btnSubmitHeader.removeAttribute("disabled");
+
+	return true;
+
 }
 
 // #validate and parse inputs
